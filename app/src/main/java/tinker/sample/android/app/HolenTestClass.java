0257a25ca89bfded6ddba0c09b01ebc1b72115be
holenzhou <790905517@qq.com>
2017-06-26 19:07:17 +0800

    将rx相关的类移出主dex

---
+++ app/src/main/java/tinker/sample/android/app/HolenTestClass.java
@@ -0,0 +1,8 @@
+package tinker.sample.android.app;
+
+/**
+ * Created by holenzhou on 2017/6/23.
+ */
+
+public class HolenTestClass {
+}
--- app/src/main/java/tinker/sample/android/app/MainActivity.java
+++ app/src/main/java/tinker/sample/android/app/MainActivity.java
@@ -30,6 +30,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tencent.tinker.lib.library.TinkerLoadLibrary;
 import com.tencent.tinker.lib.tinker.Tinker;
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         Log.e(TAG, "i am on onCreate string:" + getResources().getString(R.string.test_resource));
 //        Log.e(TAG, "i am on patch onCreate");
 
-//        Toast.makeText(this, "我改了……", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我改了……", Toast.LENGTH_SHORT).show();
 
         Button loadPatchButton = (Button) findViewById(R.id.loadPatch);
 
